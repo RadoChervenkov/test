609d9636c1d8dacf1b254fba90c84c559290ea26
rcherven <radoslav.chervenkov@telerik.com>
2015-12-14 12:00:23 +0200

    test

---
+++ ConsoleApplication1/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApplication1
+{
+    public class Car
+    {
+        public int Weight { get; set; }
+
+        public int Height { get; set; }
+
+        public Color Color { get; set; }
+
+        public FuelType Fuel { get; set; }
+    }
+}
